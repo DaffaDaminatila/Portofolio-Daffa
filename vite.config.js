1d8c6be47fb19868ae6982f55e83b8fd5c893dbe
Daffa Daminatila <gajahbobo10@gmail.com>
2025-12-11 09:40:13 +0700

    Fix image file extension case sensitivity

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portofolio-Daffa/",
+  base: "/Portofolio-Daffa/",  // <--- GANTI JADI INI (Harus sama persis dengan nama Repo)
 })
