4481386a6510c0e54b9b9e7a291638884b8fbe47
Muhammad Daffa Daminatila <91118761+DaffaDaminatila@users.noreply.github.com>
2025-12-11 09:52:09 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portofolio-Daffa/",  // <--- GANTI JADI INI (Harus sama persis dengan nama Repo)
 })
